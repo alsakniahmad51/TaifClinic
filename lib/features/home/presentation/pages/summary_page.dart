a5a2e1bfcf94ff3a3a7b005c9a48d90f22b40ecb
Ahmad <alsakniahmad51@gmail.com>
2024-12-13 23:50:31 -0500

    add share botton

--- lib/features/home/presentation/pages/summary_page.dart
+++ lib/features/home/presentation/pages/summary_page.dart
@@ -2,8 +2,12 @@ import 'package:clinic/features/home/domain/Entities/order.dart';
 import 'package:clinic/features/home/presentation/widgets/left_row_item_with_border.dart';
 import 'package:clinic/features/home/presentation/widgets/right_row_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart' as intl;
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
 
 class SummaryPage extends StatelessWidget {
   final List<Order> ordersToday;
@@ -20,7 +24,6 @@ class SummaryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // حساب عدد الطلبات لكل نوع
     final Map<String, int> orderCounts = {
       'سيفالوماتريك': 0,
       'بانوراما': 0,
@@ -35,114 +38,226 @@ class SummaryPage extends StatelessWidget {
     }
     final formattedPrice = intl.NumberFormat("#,###", "ar").format(totalPrice);
 
-    return Directionality(
-      textDirection: TextDirection.rtl,
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
+    return SingleChildScrollView(
+      child: Directionality(
+        textDirection: TextDirection.rtl,
+        child: Scaffold(
           backgroundColor: Colors.white,
-          centerTitle: true,
-          title: Column(
-            children: [
-              Text(title),
-              SizedBox(
-                height: 5.h,
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            centerTitle: true,
+            title: Column(
+              children: [
+                Text(title),
+                SizedBox(height: 5.h),
+                Text(
+                  doctorName,
+                  style: TextStyle(fontSize: 16.sp),
+                )
+              ],
+            ),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.share),
+                onPressed: () async {
+                  await _sharePdf(orderCounts, formattedPrice);
+                },
               ),
-              Text(
-                doctorName,
-                style: TextStyle(fontSize: 16.sp),
-              )
             ],
           ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'جدول الجرد:',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-              Table(
-                // border: TableBorder.all(color: Colors.black, width: 1),
-                columnWidths: const {
-                  0: FlexColumnWidth(2),
-                  1: FlexColumnWidth(2),
-                },
-                children: [
-                  TableRow(
-                    children: [
-                      RightRowItem(
-                        topRadius: 8.r,
-                        child: const Text(
-                          'نوع الطلب',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                              color: Colors.black),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      const LeftRowItemWithBorder(
-                        child: Text(
-                          'عدد الطلبات',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                              color: Colors.black),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
-                  ...orderCounts.entries.map((entry) {
-                    return TableRow(
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'جدول الجرد:',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                Table(
+                  columnWidths: const {
+                    0: FlexColumnWidth(2),
+                    1: FlexColumnWidth(2),
+                  },
+                  children: [
+                    TableRow(
                       children: [
-                        Container(
-                          height: 40.h,
-                          decoration: const BoxDecoration(
-                            color: Color(0xffE4F3E5),
+                        RightRowItem(
+                          topRadius: 8.r,
+                          child: const Text(
+                            'نوع الطلب',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.black),
+                            textAlign: TextAlign.center,
                           ),
-                          child: Center(
-                            child: Text(
-                              entry.key,
-                              textAlign: TextAlign.center,
+                        ),
+                        const LeftRowItemWithBorder(
+                          child: Text(
+                            'عدد الطلبات',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.black),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                    ...orderCounts.entries.map((entry) {
+                      return TableRow(
+                        children: [
+                          Container(
+                            height: 40.h,
+                            decoration: const BoxDecoration(
+                              color: Color(0xffE4F3E5),
+                            ),
+                            child: Center(
+                              child: Text(
+                                entry.key,
+                                textAlign: TextAlign.center,
+                              ),
                             ),
                           ),
-                        ),
+                          LeftRowItemWithBorder(
+                              child: Text(
+                            entry.value.toString(),
+                            textAlign: TextAlign.center,
+                          )),
+                        ],
+                      );
+                    }),
+                    TableRow(
+                      children: [
+                        RightRowItem(
+                            bottomRadius: 8.r,
+                            child: const Text(
+                              'إجمالي الفواتير',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center,
+                            )),
                         LeftRowItemWithBorder(
                             child: Text(
-                          entry.value.toString(),
+                          '$formattedPrice ل.س',
+                          style: const TextStyle(fontWeight: FontWeight.bold),
                           textAlign: TextAlign.center,
                         )),
                       ],
-                    );
-                  }),
-                  TableRow(
-                    children: [
-                      RightRowItem(
-                          bottomRadius: 8.r,
-                          child: const Text(
-                            'إجمالي الفواتير',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                            textAlign: TextAlign.center,
-                          )),
-                      LeftRowItemWithBorder(
-                          child: Text(
-                        '$formattedPrice ل.س',
-                        style: const TextStyle(fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.center,
-                      )),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Future<void> _sharePdf(
+      Map<String, int> orderCounts, String formattedPrice) async {
+    try {
+      final pdf = pw.Document();
+
+      final fontData = await rootBundle.load("assets/fonts/Cairo.ttf");
+      final font = pw.Font.ttf(fontData);
+
+      pdf.addPage(
+        pw.Page(
+          theme: pw.ThemeData.withFont(base: font),
+          build: (context) => pw.Directionality(
+              textDirection: pw.TextDirection.rtl,
+              child: pw.Column(
+                crossAxisAlignment: pw.CrossAxisAlignment.center,
+                children: [
+                  pw.Text(
+                    title,
+                    textDirection: pw.TextDirection.rtl,
+                    textAlign: pw.TextAlign.start,
+                    style: const pw.TextStyle(fontSize: 18),
+                  ),
+                  pw.SizedBox(height: 10),
+                  pw.Row(
+                      mainAxisAlignment: pw.MainAxisAlignment.center,
+                      children: [
+                        pw.Text(
+                          doctorName,
+                          textAlign: pw.TextAlign.start,
+                          textDirection: pw.TextDirection.rtl,
+                          style: const pw.TextStyle(fontSize: 16),
+                        ),
+                      ]),
+                  pw.SizedBox(height: 20),
+                  pw.Text(
+                    'جدول الجرد:',
+                    textDirection: pw.TextDirection.rtl,
+                    textAlign: pw.TextAlign.start,
+                    style: const pw.TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  pw.SizedBox(height: 10),
+                  pw.Table(
+                    border: pw.TableBorder.all(),
+                    columnWidths: {
+                      0: const pw.FlexColumnWidth(2),
+                      1: const pw.FlexColumnWidth(2),
+                    },
+                    children: [
+                      pw.TableRow(
+                        children: [
+                          pw.Text(
+                            'نوع الطلب',
+                            textAlign: pw.TextAlign.center,
+                            textDirection: pw.TextDirection.rtl,
+                          ),
+                          pw.Text(
+                            'عدد الطلبات',
+                            textAlign: pw.TextAlign.center,
+                            textDirection: pw.TextDirection.rtl,
+                          ),
+                        ],
+                      ),
+                      ...orderCounts.entries.map((entry) {
+                        return pw.TableRow(
+                          children: [
+                            pw.Text(
+                              entry.key,
+                              textAlign: pw.TextAlign.center,
+                              textDirection: pw.TextDirection.rtl,
+                            ),
+                            pw.Text(
+                              entry.value.toString(),
+                              textAlign: pw.TextAlign.center,
+                              textDirection: pw.TextDirection.rtl,
+                            ),
+                          ],
+                        );
+                      }),
+                      pw.TableRow(
+                        children: [
+                          pw.Text(
+                            'إجمالي الفواتير',
+                            textAlign: pw.TextAlign.center,
+                            textDirection: pw.TextDirection.rtl,
+                          ),
+                          pw.Text(
+                            '$formattedPrice ل.س',
+                            textAlign: pw.TextAlign.center,
+                            textDirection: pw.TextDirection.rtl,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              )),
+        ),
+      );
+
+      await Printing.sharePdf(bytes: await pdf.save(), filename: '$title.pdf');
+    } catch (e) {}
   }
 }
